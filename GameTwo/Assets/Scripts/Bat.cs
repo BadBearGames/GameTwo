4ac4448763c026690fe9f30b62f9dcb33311f968
Matt Dieselman <dieselmanm@gmail.com>
2016-10-17 14:03:28 -0400

    Added a bat prefab that will move in a straight line back and forth. Should work in any direction.

---
+++ GameTwo/Assets/Scripts/Bat.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bat : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public float range = 4.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update(){
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        if (transform.position.x >= range || transform.position.x <= -range || transform.position.y >= range || transform.position.y <= -range || transform.position.z >= range || transform.position.z <= -range)
+        {
+            speed = -speed;
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            // Destroy(gameObject);
+            // Destroy(collision.gameObject);
+
+            //yield return new WaitForSeconds(2);
+
+            // Instantiate(Resources.Load("Player"));
+            collision.transform.position = new Vector3(20, -.4f, .5f);
+            //Application.LoadLevel("Main");
+        }
+    }
+}
