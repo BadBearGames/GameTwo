923ea51556e65caaad0f994d501fb9d5785f6188
Matt Dieselman <dieselmanm@gmail.com>
2016-10-24 22:01:07 -0400

    Added in sinking functionality

--- GameTwo/Assets/Scripts/Platform.cs
+++ GameTwo/Assets/Scripts/Platform.cs
@@ -56,29 +56,43 @@ public class Platform : MonoBehaviour
     {
         //check if its been landed on
         float rate = 0.2f; //rate of resizing
-        float color = 2.5f;
-        this.transform.GetComponent<Renderer>().material.color = new Color( 255-(color * height),0, 0);
+        float color = (255-(height*2.5f)); //Color of the block (scales based on 100 height)
+        this.transform.GetComponent<Renderer>().material.color = new Color( color/255.0f ,0, 0); // Set new color
         if (hasLandedOn)
         {
             //shrink platform
             //transform.localScale = new Vector3(transform.localScale.x * rate, transform.localScale.y, transform.localScale.z * rate);
             height -= rate;
-            //print(height);
-            print(255 - color * height);
+            print("height: "+height);
+            print("color: "+color);
+            //print(255 - color * height);
 
         }
         else 
         {
             //check if it needs togo back to orginal siz
-            if (transform.localScale.x < orginal.x && transform.localScale.y < orginal.y && transform.localScale.z < orginal.z)
-            {
+            //if (transform.localScale.x < orginal.x && transform.localScale.y < orginal.y && transform.localScale.z < orginal.z)
+            //{
                 //grow
-                transform.localScale = new Vector3(transform.localScale.x / rate, transform.localScale.y, transform.localScale.z / rate);
+            //    transform.localScale = new Vector3(transform.localScale.x / rate, transform.localScale.y, transform.localScale.z / rate);
+            //}
+            if (height < 100)
+            {
+                height += rate;
             }
+
         }
         if (height <= 0)
         {
             //disable collider to let player fall through
+            //this.GetComponent<BoxCollider>().enabled = false;
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(20.1f, -.4f, .5f);
+            hasLandedOn = false;
+            //height = 0;
         }
+        /*else
+        {
+            //this.GetComponent<BoxCollider>().enabled = true;
+        }*/
     }
 }
