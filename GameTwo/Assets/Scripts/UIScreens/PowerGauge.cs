b897f8b081a23bf47f85b9793e6ed3f51101bab5
Satch Puri <sp1796@g.rit.edu>
2016-10-24 21:41:07 -0400

    Changed how UI looks. Added a font Added logo

--- GameTwo/Assets/Scripts/UIScreens/PowerGauge.cs
+++ GameTwo/Assets/Scripts/UIScreens/PowerGauge.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class PowerGauge : MonoBehaviour {
 
+
     public Transform LoadingBar;
 
     [SerializeField] private float currentAmount;
@@ -12,23 +13,24 @@ public class PowerGauge : MonoBehaviour {
 	// Update is called once per frame
 	void Update()
     {
+        //if mouse down
         if(Input.GetMouseButton(0))
         {
+            //and amount less than 100
             if (currentAmount < 100)
             {
+                //add amount 
                 currentAmount += speed * Time.deltaTime;
+                //For text indictors 
                 //TextIndicator.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
                 //TextLoading.gameObject.SetActive(true);
             }
-            else
-            {
-               
-                //TextLoading.gameObject.SetActive(false);
-            }
+            //fill the bar
             LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
         }
         else
         {
+            //reset
             LoadingBar.GetComponent<Image>().fillAmount = 0 ;
             currentAmount = 0;
         }
